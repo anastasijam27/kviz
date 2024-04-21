00090165d7ae2423a2120ca69ebaa57aaa5c4ae8
Anastasija Milosevic <anastasijamilosevic2005@gmail.com>
2024-04-21 13:11:29 +0200

    mala ispravka koda za registrovanje/logovanje

--- backend/server/app.py
+++ backend/server/app.py
@@ -1,11 +1,7 @@
-from flask import Flask, request,render_template, redirect,session
-from flask_sqlalchemy import SQLAlchemy
-import bcrypt
+from flask import Flask, request, render_template, redirect, session
+from db import db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app)
-app.secret_key = 'secret_key'
 
 @app.route('/')
 def index():
@@ -14,18 +10,17 @@ def index():
 @app.route('/register',methods=['GET','POST'])
 def register():
     if request.method == 'POST':
-        # handle request
         name = request.form['name']
         email = request.form['email']
         password = request.form['password']
-
-        new_user = User(name=name,email=email,password=password)
+        existing_user = User.query.filter((User.name == name) | (User.email == email)).first()
+        if existing_user:
+            return render_template('register.html', error='Username or email already exists!')
+        new_user = User(name=name,email=email)
+        new_user.set_password(password)
         db.session.add(new_user)
         db.session.commit()
         return redirect('/login')
-
-
-
     return render_template('register.html')
 
 @app.route('/login',methods=['GET','POST'])
@@ -33,24 +28,19 @@ def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-
         user = User.query.filter_by(email=email).first()
-        
         if user and user.check_password(password):
             session['email'] = user.email
             return redirect('/dashboard')
         else:
             return render_template('login.html',error='Invalid user')
-
     return render_template('login.html')
-
 
 @app.route('/dashboard')
 def dashboard():
-    if session['email']:
-        user = User.query.filter_by(email=session['email']).first()
+    if 'email' in session:
+        user = User.query.filter_by(session['email'])
         return render_template('dashboard.html',user=user)
-    
     return redirect('/login')
 
 @app.route('/logout')
--- backend/server/db.py
+++ backend/server/db.py
@@ -23,12 +23,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(130), nullable=False)
+    password = db.Column(db.String(130), nullable=False)
     questions = db.relationship('Question', backref='author', lazy=True)
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password = generate_password_hash(password)
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+        return check_password_hash(self.password, password)
     
 class UserScore(db.Model):
     id = db.Column(db.Integer, primary_key=True)
