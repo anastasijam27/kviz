098794b4e05ecc7744f4e97db74ef1e867bb6d6b
Aleksa Marjanovic <aleksamarjanovic4@gmail.com>
2024-04-21 12:24:26 +0200

    Autentifikacija korisnika (1. deo), delimicno

---
+++ backend/server/app.py
@@ -0,0 +1,62 @@
+from flask import Flask, request,render_template, redirect,session
+from flask_sqlalchemy import SQLAlchemy
+import bcrypt
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+app.secret_key = 'secret_key'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        # handle request
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+
+        new_user = User(name=name,email=email,password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect('/login')
+
+
+
+    return render_template('register.html')
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User.query.filter_by(email=email).first()
+        
+        if user and user.check_password(password):
+            session['email'] = user.email
+            return redirect('/dashboard')
+        else:
+            return render_template('login.html',error='Invalid user')
+
+    return render_template('login.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    if session['email']:
+        user = User.query.filter_by(email=session['email']).first()
+        return render_template('dashboard.html',user=user)
+    
+    return redirect('/login')
+
+@app.route('/logout')
+def logout():
+    session.pop('email',None)
+    return redirect('/login')
+
+if __name__ == '__main__':
+    app.run(debug=True)
